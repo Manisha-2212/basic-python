28d585f6701115c56bdb79a82d1f40cbca2a1616
Manisha-2212 <113374433+Manisha-2212@users.noreply.github.com>
2022-09-13 16:08:32 +0530

    Create while_statement.py

---
+++ while_statement.py
@@ -0,0 +1,26 @@
+# while Loop
+
+i = 1
+while i < 6:
+    print (i)
+    i += 1
+print("while loop executed successfully")
+
+# break statement
+# to stop loop execution
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+    if i == 3:
+        break
+print("demo for break statement")
+
+#continue statement
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+    if i == 3:
+        continue
+print("demo for continue statement")
